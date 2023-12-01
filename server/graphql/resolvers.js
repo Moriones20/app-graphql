5ce15fd9fcedc69f7855e81381599cf380d335f7
Moriones20 <moriones06082001@gmail.com>
2023-12-01 15:00:49 -0500

    List tasks(Queries)

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,14 +1,27 @@
 import Project from "../models/project.js";
+import Task from "../models/task.js";
 
 export const resolvers = {
   Query: {
     hello: () => "Hello world!",
+    projects: async () => await Project.find(),
+    tasks: async () => await Task.find(),
   },
   Mutation: {
     createProject: async (_, { name, description }) => {
       const project = new Project({ name, description });
       const savedProject = await project.save();
       return savedProject;
+    },
+    createTask: async (_, { title, projectId }) => {
+      const projectExists = await Project.findById(projectId);
+      if (!projectExists) {
+        throw new Error("Project not found");
+      }
+
+      const task = new Task({ title, projectId });
+      const savedTask = await task.save();
+      return savedTask;
     },
   },
 };
