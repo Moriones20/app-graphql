a8310a2561e9feaefc50a080188a3328628bfa05
Moriones20 <moriones06082001@gmail.com>
2023-12-01 15:30:42 -0500

    Add delete mutation

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -5,13 +5,23 @@ export const resolvers = {
   Query: {
     hello: () => "Hello world!",
     projects: async () => await Project.find(),
+    project: async (_, { _id }) => await Project.findById(_id),
     tasks: async () => await Task.find(),
+    task: async (_, { _id }) => await Task.findById(_id),
   },
   Mutation: {
     createProject: async (_, { name, description }) => {
       const project = new Project({ name, description });
       const savedProject = await project.save();
       return savedProject;
+    },
+    deleteProject: async (_, { _id }) => {
+      const deletedProject = await Project.findByIdAndDelete(_id);
+      if (!deletedProject) {
+        throw new Error("Project not found");
+      }
+
+      return deletedProject;
     },
     createTask: async (_, { title, projectId }) => {
       const projectExists = await Project.findById(projectId);
@@ -22,6 +32,14 @@ export const resolvers = {
       const task = new Task({ title, projectId });
       const savedTask = await task.save();
       return savedTask;
+    },
+    deleteTask: async (_, { _id }) => {
+      const deletedTask = await Task.findByIdAndDelete(_id);
+      if (!deletedTask) {
+        throw new Error("Task not found");
+      }
+
+      return deletedTask;
     },
   },
 };
