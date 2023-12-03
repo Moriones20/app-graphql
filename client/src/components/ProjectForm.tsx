049776df70862b1d109a96f51061f9ce8b4a6e7f
Moriones20 <moriones06082001@gmail.com>
2023-12-03 15:50:37 -0500

    Add: create project

--- client/src/components/ProjectForm.tsx
+++ client/src/components/ProjectForm.tsx
@@ -1,5 +1,64 @@
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { CREATE_PROJECT, GET_PROJECTS } from "../graphql/projects";
+
 function ProjectForm() {
-  return <div>project-form</div>;
+  const [project, setProject] = useState({
+    name: "",
+    description: "",
+  });
+
+  const [createProject, { loading, error }] = useMutation(CREATE_PROJECT, {
+    refetchQueries: [
+      {
+        query: GET_PROJECTS,
+      },
+      "GetProjects",
+    ],
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+
+    setProject({
+      ...project,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createProject({
+      variables: {
+        name: project.name,
+        description: project.description,
+      },
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {error && <p>{error.message}</p>}
+
+      <input
+        type="text"
+        name="name"
+        placeholder="Write a title"
+        onChange={handleChange}
+      />
+      <textarea
+        name="description"
+        rows={3}
+        placeholder="Write a description"
+        onChange={handleChange}
+      ></textarea>
+      <button disabled={!project.name || !project.description || loading}>
+        Save
+      </button>
+    </form>
+  );
 }
 
 export default ProjectForm;
